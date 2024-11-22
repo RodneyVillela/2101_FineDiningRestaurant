36c9e3c1cc4786c1b304e1b4c0620fe086208066
kyla0403 <kylaa7070@gmailcom>
2024-11-22 20:37:45 +0800

    Menu, HomePage, Login

---
+++ src/Controller_Connector/DBConnect.java
@@ -0,0 +1,41 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package Controller_Connector;
+
+import java.sql.*;
+import javax.swing.JOptionPane;
+
+//class for DB Connection
+public class DBConnect {
+    
+    private static String url = "jdbc:mysql://localhost:3306/lameza_restaurant";
+    private static String user = "root";
+    private static String userPassword = "";
+    
+    private static Connection con;
+    private static DBConnect dbCon;
+    
+    public DBConnect(){
+        //Get Connection
+        con = null;
+        try {
+            con =  DriverManager.getConnection(url, user, userPassword);
+            JOptionPane.showMessageDialog(null, "Connected Successfully");
+        } catch(Exception e){
+            JOptionPane.showMessageDialog(null, "Connection Error"+ e.getMessage());
+        }
+               
+    }
+    public static DBConnect getDBConnection(){
+        if(dbCon == null){
+            dbCon = new DBConnect();
+        }
+        return dbCon;
+    }
+    public static Connection getConnection(){
+        return con;
+    }
+    
+}
